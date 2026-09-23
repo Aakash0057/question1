933e23761cccfc878b2f9bde2f5b570de54f7aec
Aakash <aakashkarthikeyan1999@gmail.com>
2026-09-23 16:08:15 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git branch: 'main', url: 'https://github.com/Aakash0057/Project1.git'
+                git branch: 'main', url: 'https://github.com/Aakash0057/question1.git'
             }
         }
         stage('Install Dependencies') {
